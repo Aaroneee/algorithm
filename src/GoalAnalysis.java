beee478aa47968ed06317f72b6b82e8a4a9b88ac
Aaron <827052236@qq.com>
2020-12-09 16:45:47 +0800

    1678. 设计 Goal 解析器

---
+++ src/GoalAnalysis.java
@@ -0,0 +1,18 @@
+public class GoalAnalysis {
+
+    /**
+     * describe: 1678. 设计 Goal 解析器
+     *
+     * @Param:
+     * @Return:
+     * @Author: Aaron
+     * @Date: 2020/12/9 16:44
+     */
+    public static void main(String[] args) {
+        System.out.println("interpret() = " + interpret("(al)G(al)()()G"));
+    }
+
+    public static String interpret(String command) {
+        return command.replace("()", "o").replace("(al)", "al");
+    }
+}
