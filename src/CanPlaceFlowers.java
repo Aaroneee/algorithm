c671c18519a507088300a6c85b38ac32f08d22bd
Aaron <827052236@qq.com>
2021-01-02 11:34:21 +0800

    605. 种花问题

---
+++ src/CanPlaceFlowers.java
@@ -0,0 +1,34 @@
+/**
+ * describe: 605. 种花问题
+ *
+ * @Author: Aaron
+ * @Date: 2021/1/2 11:31
+ */
+public class CanPlaceFlowers {
+    public static void main(String[] args) {
+        int[] flowerbed = {1, 0, 0, 1, 0, 1};
+//        int[] flowerbed={0,0,1,0,1};
+        int n = 1;
+        System.out.println("solution = " + solution(flowerbed, n));
+    }
+
+    public static boolean solution(int[] flowerbed, int n) {
+        int num = 0;
+        int length = flowerbed.length;
+        for (int i = 0; i < length; i += 2) {
+            if (flowerbed[i] == 0) {
+                //判断是否是最后一格
+                if (i == length - 1 || flowerbed[i + 1] == 0) {
+                    num++;
+                } else {
+                    i++;
+                }
+            }
+        }
+        return n <= num;
+    }
+}
+
+
+
+
